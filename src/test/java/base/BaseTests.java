99447ab53a8ee78439bd181cc7890acc469df785
ronnika <lunkestr@gmail.com>
2022-09-09 14:52:43 +0300

    add findingElements test

--- src/test/java/base/BaseTests.java
+++ src/test/java/base/BaseTests.java
@@ -1,20 +1,45 @@
 package base;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class BaseTests {
-    private WebDriver driver;
+import java.util.List;
 
+public class BaseTests {
+    private static WebDriver driver;
+
+    @BeforeAll
+    public static void init() {
+        System.setProperty("webdriver.chrome.driver", "src/resources/chromedriver");
+    }
     @Test
     public void setUp() {
-        System.setProperty("webdriver.chrome.driver", "src/resources/chromedriver");
         driver = new ChromeDriver();
         driver.get("https://the-internet.herokuapp.com/");
-        driver.manage().window().setSize(new Dimension(375, 812));
-        System.out.println(driver.getTitle());
+//        driver.manage().window().setSize(new Dimension(375, 812));
+//        WebElement inputsLink = driver.findElement(By.linkText("Inputs"));
+//        inputsLink.click();
+//        List<WebElement> links = driver.findElements(By.tagName("a"));
+//        System.out.println(links.size());
+//        System.out.println(driver.getTitle());
+        WebElement shiftingContentLink = driver.findElement(By.linkText("Shifting Content"));
+        shiftingContentLink.click();
+        WebElement menuLink = driver.findElement(By.linkText("Example 1: Menu Element"));
+        menuLink.click();
+        List<WebElement> menuElements = driver.findElements(By.cssSelector("div.example li"));
+        System.out.println(menuElements.size());
+    }
+
+    @AfterEach
+    public void tearDown() {
         driver.quit();
     }
+
 }
---
+++ src/test/java/course/findingWebElementsTests.java
@@ -0,0 +1,16 @@
+package course;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
+
+public class findingWebElementsTests {
+    @Disabled
+    @Test
+    void findLink() {
+
+    }
+}
