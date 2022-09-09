c86a5d31c45bd459a420978b09ff582e95978f77
ronnika <lunkestr@gmail.com>
2022-09-09 17:57:50 +0300

    refactor

--- src/test/java/login/LoginTests.java
+++ src/test/java/login/LoginTests.java
@@ -10,10 +10,10 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 public class LoginTests extends BaseTests {
     @Test
     public void testSuccessfulLogin() {
-        LoginPage loginPage = homePage.clickFormAuthentication();
+        var loginPage = homePage.clickFormAuthentication();
         loginPage.setUserName("tomsmith");
         loginPage.setPassword("SuperSecretPassword!");
-        SecureAreaPage secureAreaPage = loginPage.clickLoginButton();
+        var secureAreaPage = loginPage.clickLoginButton();
         assertTrue(secureAreaPage.getAlertText().contains("You logged into a secure area!"));
     }
 }
