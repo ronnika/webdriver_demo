9e156a7c80ac19bc4ea1b0e23b3f5cc075f1f016
ronnika <lunkestr@gmail.com>
2022-09-09 17:58:12 +0300

    add dropdown test

--- src/main/java/pages/HomePage.java
+++ src/main/java/pages/HomePage.java
@@ -6,14 +6,23 @@ import org.openqa.selenium.WebDriver;
 public class HomePage {
     private WebDriver driver;
 
-    private By formAuthenticationLink = By.linkText("Form Authentication");
+    private By dropdownLink = By.linkText("Form Authentication");
 
     public HomePage(WebDriver driver) {
         this.driver = driver;
     }
 
+    private void clickLink(String linkText) {
+        driver.findElement(By.linkText(linkText)).click();
+    }
+
     public LoginPage clickFormAuthentication() {
-        driver.findElement(formAuthenticationLink).click();
+        clickLink("Form Authentication");
         return new LoginPage(driver);
+    }
+
+    public DropdownPage clickDropdown() {
+        clickLink("Dropdown");
+        return new DropdownPage(driver);
     }
 }
---
+++ src/test/java/dropdown/DropdownTests.java
@@ -0,0 +1,22 @@
+package dropdown;
+
+import base.BaseTests;
+import org.junit.jupiter.api.Test;
+import pages.DropdownPage;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class DropdownTests extends BaseTests {
+
+    @Test
+    public void testSelectedOption() {
+        var dropdownPage = homePage.clickDropdown();
+
+        String option = "Option 1";
+        dropdownPage.selectFromDropdown(option);
+        var selectedOptions = dropdownPage.getSelectedOptions();
+        assertEquals(1, selectedOptions.size(), "Incorrect number of options");
+        assertTrue(selectedOptions.contains(option), "Option not selected");
+
+    }
+}
