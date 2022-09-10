55cc8df793c19a77ce056aea70187e59fb1ef110
ronnika <lunkestr@gmail.com>
2022-09-10 21:46:33 +0300

    optimize imports

--- src/test/java/base/BaseTests.java
+++ src/test/java/base/BaseTests.java
@@ -1,14 +1,11 @@
 package base;
 
-import org.junit.jupiter.api.*;
-import org.openqa.selenium.By;
-import org.openqa.selenium.Dimension;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import pages.HomePage;
-
-import java.util.List;
 
 public class BaseTests {
     private static WebDriver driver;
