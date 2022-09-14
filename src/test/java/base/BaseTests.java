7f8c131172ad66be2242458b256837171e8f4c44
ronnika <lunkestr@gmail.com>
2022-09-14 16:53:44 +0300

    add screenshots

--- src/test/java/base/BaseTests.java
+++ src/test/java/base/BaseTests.java
@@ -1,16 +1,25 @@
 package base;
 
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
+import com.google.common.io.Files;
+import jdk.jfr.Description;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.extension.TestWatcher;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import pages.HomePage;
 import utils.WindowManager;
 
+import java.io.File;
+import java.io.IOException;
+
 public class BaseTests {
     private static WebDriver driver;
     protected static HomePage homePage;
+
+
+
 
     @BeforeAll
     public static void init() {
@@ -24,6 +33,18 @@ public class BaseTests {
     @BeforeEach
     public void goHome() {
         driver.get("https://the-internet.herokuapp.com/");
+    }
+
+    @AfterEach
+    public void takeScreenshot() {
+        var camera = (TakesScreenshot)driver;
+        File screenshot = camera.getScreenshotAs(OutputType.FILE);
+        System.out.println("Screenshot taken: " + screenshot.getAbsolutePath());
+        try {
+            Files.move(screenshot, new File("src/resources/screenshots/test.png"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @AfterAll
